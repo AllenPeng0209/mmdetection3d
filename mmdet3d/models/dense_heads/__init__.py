b3e0b390d5016d3048d5ab0e6928a973f8e7fb14
yanlunpeng <allenpeng0209@gmail.com>
2020-08-25 14:52:44 +0800

    deeproute dataset

--- mmdet3d/models/dense_heads/__init__.py
+++ mmdet3d/models/dense_heads/__init__.py
@@ -2,8 +2,7 @@ from .anchor3d_head import Anchor3DHead
 from .free_anchor3d_head import FreeAnchor3DHead
 from .parta2_rpn_head import PartA2RPNHead
 from .vote_head import VoteHead
-from .center3d_head import Center3DHead
 
 
 
-__all__ = ['Anchor3DHead', 'FreeAnchor3DHead', 'PartA2RPNHead', 'VoteHead', 'Center3DHead']
+__all__ = ['Anchor3DHead', 'FreeAnchor3DHead', 'PartA2RPNHead', 'VoteHead']
