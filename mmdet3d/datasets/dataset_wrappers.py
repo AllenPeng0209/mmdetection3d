a905b1e6630708af2a9f7a44dae13ff546a5021f
yanlunpeng <allenpeng0209@gmail.com>
2020-08-28 09:54:59 +0800

    dataset wrappers

--- mmdet3d/datasets/dataset_wrappers.py
+++ mmdet3d/datasets/dataset_wrappers.py
@@ -36,7 +36,6 @@ class ClassSampledDataset(object):
         """
         data = mmcv.load(ann_file)
         _cls_inds = {name: [] for name in self.CLASSES}
-        embed()
         for idx, info in enumerate(data['infos']):
             if self.dataset.use_valid_flag:
                 mask = info['valid_flag']
