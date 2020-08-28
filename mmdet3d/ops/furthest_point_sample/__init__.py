70dfd495d41d056a14c75720114ebf2ffecf138a
yanlunpeng <allenpeng0209@gmail.com>
2020-08-28 14:48:40 +0800

    3dssd

--- mmdet3d/ops/furthest_point_sample/__init__.py
+++ mmdet3d/ops/furthest_point_sample/__init__.py
@@ -1,4 +1,4 @@
-from .furthest_point_sample import (furthest_point_sample,
-                                    furthest_point_sample_with_dist)
+from .furthest_point_sample import (furthest_point_sample,furthest_point_sample_with_dist)
+
 
 __all__ = ['furthest_point_sample', 'furthest_point_sample_with_dist']
